f9339e49dae97029369d4d92244e72497c33b299
23navi <navisureka23@gmail.com>
2024-02-17 09:55:06 +0530

    simple llm

--- snip/main.py
+++ snip/main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import os
+from langchain.llms import OpenAI
+
+api_key= os.environ.get("OPENAI_API_KEY","default")
+print(f"OpenAI API key {api_key}")
+
+llm = OpenAI(
+      openai_api_key= api_key
+)
+
+result = llm("Write a very short poem")
+
+print(result)
