1af3c6600bbd1deb6dbb5e6766a18c760b389e57
23navi <navisureka23@gmail.com>
2024-02-21 00:01:26 +0530

    worker service

--- pdf.ai/app/chat/vector_stores/pinecone.py
+++ pdf.ai/app/chat/vector_stores/pinecone.py
@@ -5,6 +5,9 @@ from app.chat.embeddings.openai import embeddings
 
 pinecone.init(
       api_key=os.getenv('PINECONE_API_KEY'),
-      environment=os.getenv("")
+      environment=os.getenv("PINECONE_ENV_NAME")
 )
 
+vector_store= Pinecone.from_existing_index(
+      os.getenv("PINECONE_INDEX_NAME"),embeddings
+)
