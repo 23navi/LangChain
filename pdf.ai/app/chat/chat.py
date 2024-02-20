04580ea1ef2ce4c514e7cc05290811749de1536f
23navi <navisureka23@gmail.com>
2024-02-21 01:28:08 +0530

    streaming

--- pdf.ai/app/chat/chat.py
+++ pdf.ai/app/chat/chat.py
@@ -1,4 +1,5 @@
 # from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
@@ -9,10 +10,12 @@ from app.chat.memories.sql_memory import build_memory
 def build_chat(chat_args: ChatArgs):
     retriever= build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming = False)
     memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
     llm=llm,
     retriever= retriever,
-    memory=memory
+    memory=memory,
+    condense_question_llm=condense_question_llm
     )
